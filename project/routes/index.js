90af8a674114f74daf2d9a2ad5d022dcfb6ee8d3
bexredd <r.redd94@gmail.com>
2017-11-16 00:42:34 +0000

    routes

--- project/routes/index.js
+++ project/routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Pictures = mongoose.model('Pictures');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.get('/post', function(req, res, next) {
+  Pictures.find(function(err,pictures){
+	if(err){return next(err);}
+  	res.json(pictures);
+  });
+});
+
+router.post('/post', function(req,res,next){
+	var pictures = new Pictures(req.body);
+	pictures.save(function(err,pictures){
+		if(err){return next(err);}
+		res.json(pictures);
+	});
 });
 
 module.exports = router;
